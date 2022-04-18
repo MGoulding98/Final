8d1cee92f505a7b33c92b2d205f5db8be04f5280
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-04-18 10:58:30 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
